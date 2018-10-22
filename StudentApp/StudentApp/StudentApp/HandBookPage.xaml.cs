7086bc7ee347105ddb73795aad359b684f1135ae
HAPTXSE62665\haptxse62665 <ha.pham@wisky.vn>
2018-10-22 10:29:29 +0700

    haptx

---
+++ StudentApp/StudentApp/StudentApp/HandBookPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace StudentApp
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class HandBookPage : ContentPage
+	{
+		public HandBookPage ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
