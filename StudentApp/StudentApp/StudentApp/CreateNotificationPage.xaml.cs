1dd84d257c26d79cbf64b54fa7782444aab449b6
HAPTXSE62665\haptxse62665 <ha.pham@wisky.vn>
2018-10-22 10:20:40 +0700

    haptx

---
+++ StudentApp/StudentApp/StudentApp/CreateNotificationPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace StudentApp
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class CreateNotificationPage : ContentPage
+    {
+        public CreateNotificationPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
